9e09b363b6f6b259188367e56adea5a9c88c6872
Ivan Lavena <lavenaivan@gmail.com>
2020-10-26 18:43:39 -0600

    skeleton

--- javascript/client.js
+++ javascript/client.js
@@ -1,60 +1,40 @@
 function init(){
+  
+  /*
+ * Noel Delgado | @pixelia_me
+ *
+*/
 
-  
-  
-  console.clear();
-  const svg = document.querySelector("#demo");
-  const tl = new TimelineMax({onUpdate:onUpdate});
-  let pt = svg.createSVGPoint();
-  let data = document.querySelector(".tlProgress");
-  let counter = document.querySelector("#counter");
-  const ratio = 0.5625;
-  
-  gsap.set("#instructions, #dial", {xPercent: -50});
-  gsap.set("#progressRing", {drawSVG:0});
-  
-  tl.to("#masker", {duration: 2, attr:{r:2400}, ease:"power2.in"});
-  tl.reversed(true);
-  
-  function mouseHandler() {
-  tl.reversed(!tl.reversed());
+var svgElement = document.querySelector('svg');
+var maskedElement = document.querySelector('#mask-circle');
+var circleFeedback = document.querySelector('#circle-shadow');
+var svgPoint = svgElement.createSVGPoint();
+
+function cursorPoint(e, svg) {
+    svgPoint.x = e.clientX;
+    svgPoint.y = e.clientY;
+    return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
 }
 
-function getPoint(evt){
-  pt.x = evt.clientX; 
-  pt.y = evt.clientY;
-  return pt.matrixTransform(svg.getScreenCTM().inverse());
+function update(svgCoords) {
+    maskedElement.setAttribute('cx', svgCoords.x);
+    maskedElement.setAttribute('cy', svgCoords.y);
+    circleFeedback.setAttribute('cx', svgCoords.x);
+    circleFeedback.setAttribute('cy', svgCoords.y);
 }
 
-function mouseMove(evt) {
-  let newPoint = getPoint(evt);
-  gsap.set("#dot", {attr:{cx:newPoint.x, cy:newPoint.y}});
-  gsap.to("#ring, #masker", 0.88, {attr:{cx:newPoint.x, cy:newPoint.y}, ease:"power2.out"});
- }
- 
- function onUpdate() {
-  let prog = (tl.progress() * 100);
-  gsap.set("#progressRing", {drawSVG:prog + "%"});
-  counter.textContent = prog.toFixed();
+window.addEventListener('mousemove', function(e) {
+  update(cursorPoint(e, svgElement));
+}, false);
+
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) {
+        update(cursorPoint(touch, svgElement));
+    }
+}, false);  
+
 }
 
-function newSize() {
-  let w = window.innerWidth ;
-  let h = window.innerHeight;
-  if (w > h * (16/9) ) {
-    gsap.set("#demo", { attr: { width: w, height: w * ratio } });
-  } else {
-    gsap.set("#demo", { attr: { width: h / ratio, height: h } });
-  }
-  let data = svg.getBoundingClientRect();
-  gsap.set("#demo", {x:w/2 - data.width/2});
-  gsap.set("#demo", {y:h/2 - data.height/2});
-}
-
-window.addEventListener("mousedown", mouseHandler);
-window.addEventListener("mouseup", mouseHandler);
-window.addEventListener("mousemove", mouseMove);
-
-newSize();
-window.addEventListener("resize", newSize);
-}
+window.addEventListener('load', init);
